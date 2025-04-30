eb8b75dec3231829bf46d2f24792e208802aaa0d
Felisha Gibbs <Felishagibbs3@gmail.com>
2025-04-29 23:36:55 -0500

    AccountingLedger Update

---
+++ src/main/java/com/pluralsight/OnlineStore.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class OnlineStore {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] inventory = {"Ergonomic Keyboard", "Mouse", "Monitor", "USB Cable", "Laptop Stand"};
+        double[] price = {89.50, 15.99, 199.99, 9.00, 35.00};
+    }
+}
